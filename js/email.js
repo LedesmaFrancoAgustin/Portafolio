e17cd2faaa66c8e54351f1a65f4bad2f434c6366
LedesmaFrancoAgustin <ledesmafranco50@gmail.com>
2026-01-12 14:41:33 -0300

    Modificacion Responsive.v1.2

--- js/email.js
+++ js/email.js
@@ -2,12 +2,19 @@
 const formEmail = document.getElementById('formEmail');
 const buttonForm = document.getElementById('buttonForm');
 
+ //const serviceID = 'service_lqf4uas';
+ //const templateID = 'template_gmkl5lh';
+ //const apikey = 'cy6blbeOu17XpocIC';
+
+  const serviceID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+  const templateID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+  const apikey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+
+
 formEmail.addEventListener('submit', function (event) {
   event.preventDefault();
 
-   const serviceID = 'service_lqf4uas';
-   const templateID = 'template_gmkl5lh';
-   const apikey = 'cy6blbeOu17XpocIC';
+  
 
   emailjs.init(serviceID);
 
